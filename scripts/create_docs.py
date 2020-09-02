040fc060b4918638da597b8b92f0d81e4c47aca9
Raul <rchfe23@gmail.com>
2020-09-01 22:06:12 -0400

    create_docs: change tag name to <host name=> and change variable names to use host instead of site

--- scripts/create_docs.py
+++ scripts/create_docs.py
@@ -12,72 +12,72 @@ from pyutils.genutils import read_file, write_file
 
 
 VERSION = "0.0.1a0"
-TAG_NAME = "site"
-README_FILE_EXT = ['rst']
+TAG_NAME = "host"
+DOC_FILE_EXT = ['rst']
 TEMPLATE_FILE_EXT_PREFIX = "t_"
-TEMPLATE_FILE_EXT = [TEMPLATE_FILE_EXT_PREFIX + t for t in README_FILE_EXT]
+TEMPLATE_FILE_EXT = [TEMPLATE_FILE_EXT_PREFIX + t for t in DOC_FILE_EXT]
 DEST_VARIABLE_PREFIX = "filename_"
-# Websites where the READMEs will be published, e.g. GitHub
+# Hosts where the docs will be published, e.g. GitHub
 # Default filename without extension
-WEBSITES = {
+HOSTS = {
     'github': {
         'default_filename': "README_github",
         'long_opt_name': "github",
-        'site_name': "GitHub",
+        'hostname': "GitHub",
     },
     'pypi': {
         'default_filename': "README_pypi",
         'long_opt_name': "pypi",
-        'site_name': "PyPI",
+        'hostname': "PyPI",
     },
     'readthedocs': {
         'default_filename': "README_docs",
         'long_opt_name': "docs",
-        'site_name': "readthedocs",
+        'hostname': "readthedocs",
     },
 }
 
 
-# Assume template filepath and README filepaths are valid, already checked
-def _create_readme(template_filepath, dst_dirpath, websites):
+# Assume template and docs filepaths are valid, already checked
+def _create_docs(template_filepath, dst_dirpath, hosts):
     """
 
     Parameters
     ----------
     template_filepath
     dst_dirpath
-    websites
+    hosts
 
     Returns
     -------
 
     """
     orig_txt = read_file(template_filepath)
-    regex1 = r"<{}=\".*?\">.*?<\/{}>".format(TAG_NAME, TAG_NAME)
-    # TODO: use threads for each site
-    # TODO: add include <site> tags
-    for s in websites:
-        website_key = s[0]
-        readme_filename = s[1]
-        _, readme_ext = split_fname(readme_filename)
+    regex1 = r"<{} name=\".*?\">.*?<\/{}>".format(TAG_NAME, TAG_NAME)
+    # TODO: use threads for each host
+    for s in hosts:
+        hostname = s[0]
+        doc_filename = s[1]
+        _, doc_ext = split_fname(doc_filename)
         # Not necessary (since we assume input is cleaned) but just in case
-        assert readme_ext, \
-            "The README filename '{}' doesn't have any extension".format(
-                readme_filename)
+        assert doc_ext, \
+            "The doc filename '{}' doesn't have any extension".format(
+                doc_filename)
         copy_txt = copy.copy(orig_txt)
         while True:
             # TODO: give better variable names
             match1 = re.search(regex1, copy_txt, re.DOTALL | re.MULTILINE)
             if match1:
-                site_tag = match1.group()
-                regex_open_site_tag = r"<{}=\".*?\">".format(TAG_NAME)
-                match2 = re.search(regex_open_site_tag, site_tag,
+                tag = match1.group()
+                regex_open_tag = r"<{} name=\".*?\">".format(TAG_NAME)
+                match2 = re.search(regex_open_tag, tag,
                                    re.DOTALL | re.MULTILINE)
-                if website_key in match2.group():
-                    close_site_tag = "</{}>".format(TAG_NAME)
+                if hostname in match2.group():
+                    # Case: tags related to selected host
+                    close_tag = "</{}>".format(TAG_NAME)
                     first = copy_txt[:match1.start()]
                     middle_start_pos = match1.start()+match2.end()
-                    middle_end_pos = match1.end()-len(close_site_tag)
+                    middle_end_pos = match1.end()-len(close_tag)
                     if copy_txt[middle_start_pos] == '\n':
                         # Remove \n, i.e. from open and close tags
                         middle_start_pos += 1
@@ -86,19 +86,19 @@ def _create_readme(template_filepath, dst_dirpath, websites):
                     last = copy_txt[match1.end():]
                     copy_txt = first + middle + last
                 else:
-                    # import ipdb
-                    # ipdb.set_trace()
+                    # Case: removing tags and their content because not related
+                    # to selected host
                     if copy_txt[match1.start()-1] == '\n' and \
                             (len(copy_txt) == match1.end() or  # end of file
                              copy_txt[match1.end()] == '\n'):
-                        # TODO: do a regex search
                         match1_end = match1.end() + 1
                     else:
                         match1_end = match1.end()
                     copy_txt = copy_txt[:match1.start()] + copy_txt[match1_end:]
             else:
                 break
-        write_file(readme_filename, copy_txt)
+        doc_filepath = os.path.join(dst_dirpath, doc_filename)
+        write_file(doc_filepath, copy_txt)
     return 0
 
 
@@ -130,40 +130,40 @@ def setup_argparser():
     # TODO: add overwrite option
     parser.add_argument("-v", "--version", action='version',
                         version='%(prog)s {}'.format(VERSION))
-    site_names = ", ".join([s for s in WEBSITES.keys()])
+    hostnames = ", ".join([s for s in HOSTS.keys()])
     parser.add_argument(
         "-t", "--template", dest="template_filepath",
         required=True,
-        help='''File path to the template used for generating the READMEs from
-        different sites ({})'''.format(site_names))
+        help='''File path to the template used for generating the docs from 
+        different hosts ({})'''.format(hostnames))
     parser.add_argument(
         "-d", "--dst-dirpath", default=None, dest="dst_dirpath",
-        help='''Directory path where the READMEs ({}) will be saved. By 
+        help='''Directory path where the docs ({}) will be saved. By 
         default, they are saved in the directory where the script is 
-        executed.'''.format(", ".join(README_FILE_EXT)))
+        executed.'''.format(", ".join(DOC_FILE_EXT)))
     short_opts = []
-    for k, v in WEBSITES.items():
-        site_name = v['site_name']
-        site_filename = v['default_filename']
-        if len(README_FILE_EXT) == 1:
-            default = "{}.{}".format(site_filename, README_FILE_EXT[0])
+    for k, v in HOSTS.items():
+        hostname = v['hostname']
+        doc_filename = v['default_filename']
+        if len(DOC_FILE_EXT) == 1:
+            default = "{}.{}".format(doc_filename, DOC_FILE_EXT[0])
         else:
-            default = "README_{}"
+            default = "{}. {{{}}}".format(doc_filename, ", ".join(DOC_FILE_EXT))
         dest = "{}{}".format(DEST_VARIABLE_PREFIX, k)
         nargs = "?"
-        help_ = '''{}'s README filename.'''.format(site_name)
-        first_letter_name = site_name[0][0].lower()
+        help_ = '''{}'s doc filename.'''.format(hostname)
+        first_letter_name = hostname[0][0].lower()
         parameters = dict(dest=dest,
                           default=default,
                           nargs=nargs,
                           help=help_)
         if first_letter_name in short_opts:
-            parser.add_argument("--{}".format(site_name.lower()), **parameters)
+            parser.add_argument("--{}".format(hostname.lower()), **parameters)
         else:
-            short_opts.append(site_name[0].lower())
+            short_opts.append(hostname[0].lower())
             parser.add_argument(
                 "-{}".format(first_letter_name),
-                "--{}".format(site_name.lower()),
+                "--{}".format(hostname.lower()),
                 **parameters)
     return parser.parse_args()
 
@@ -194,10 +194,12 @@ def main():
     retcode = 0
     try:
         args = setup_argparser()
+        if args.dst_dirpath is None:
+            args.dst_dirpath = ""
         dict_vars = vars(args)
-        websites = []
+        hosts = []
         _, template_ext = split_fname(args.template_filepath)
-        readme_ext = template_ext.split(TEMPLATE_FILE_EXT_PREFIX)[1]
+        doc_ext = template_ext.split(TEMPLATE_FILE_EXT_PREFIX)[1]
         assert template_ext in TEMPLATE_FILE_EXT, \
             "Wrong template file extension: {}. Valid template file extensions " \
             "are: {}".format(os.path.basename(args.template_filepath),
@@ -205,29 +207,28 @@ def main():
         for dest, arg_value in dict_vars.items():
             if dest.startswith(DEST_VARIABLE_PREFIX):
                 fname = arg_value
-                key = dest.lstrip(DEST_VARIABLE_PREFIX)
+                hostname = dest.lstrip(DEST_VARIABLE_PREFIX)
                 if arg_value:
                     fname_root, fname_ext = split_fname(fname)
-                    assert fname_ext == readme_ext, \
-                        "Template '{}' and README file '{}' are not " \
+                    assert fname_ext == doc_ext, \
+                        "Template '{}' and doc file '{}' are not " \
                         "compatible. Check their file extensions.".format(
                             os.path.basename(args.template_filepath),
                             fname)
                 else:
-                    fname = WEBSITES[key]['default_filename']
+                    fname = HOSTS[hostname]['default_filename']
                     assert "." not in fname, \
                         "The default filename '{}' should not have an " \
                         "extension".format(fname)
-                    fname_root = fname
-                    fname += ".{}".format(readme_ext)
+                    fname += ".{}".format(doc_ext)
                 if arg_value is None or fname in sys.argv:
-                    websites.append((key, fname))
+                    hosts.append((hostname, fname))
         # =======
         # Actions
         # =======
-        if websites:
-            retcode = _create_readme(args.template_filepath, args.dst_dirpath,
-                                     websites)
+        if hosts:
+            retcode = _create_docs(args.template_filepath, args.dst_dirpath,
+                                   hosts)
     except SystemExit:
         retcode = 1
     except Exception:
